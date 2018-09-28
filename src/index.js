99f1dec38417ca2734f2cc88cfe3ccd776fae314
Drizzlman <kovalko.alexey@yandex.by>
2018-09-28 22:46:59 +0300

    Task_7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    var a = 0;
+    var b = number/5;
+    while (b > 0) {
+        a = a+ Math.floor(b);
+        b =b/5;
+    }
+    return a;
 }
